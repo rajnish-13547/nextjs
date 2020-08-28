8848d25cb8e86f737df9ae282277b8510e23c395
Rajnish Singh <41592584+rajnish-13547@users.noreply.github.com>
2020-08-28 20:47:48 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,42 +15,18 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing : 
-          <code className={styles.code}>https://rajnish-aiml.com</code>
+          Thank you for visiting{' '}
+          <code className={styles.code}>https://rajnish-aiml.com</code> !
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>About &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>About &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
+          <a 
+            href="https://nextjs.org/learn" 
             className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+           >
+            <h3>Projects &rarr;</h3>
+            <p>Data Science Projects done by me: </p>
           </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            
         </div>
       </main>
 
